c729bf1a54a56ce8e7a2b1947b3e26308636e02a
Pablo Raemdonck <praemdonck@gmail.com>
2025-01-16 23:17:48 +0000

    Multiple changes

--- sudoku_board.js
+++ sudoku_board.js
@@ -1,48 +1,40 @@
 const sudokuBoard = document.getElementById("sudokuBoard");
 sudokuBoard.addEventListener("contextmenu", e => e.preventDefault());
+const body = document.getElementById("body");
+body.addEventListener("contextmenu", e => e.preventDefault());
 
 function generateSudokuCells(box, boxHeight) {
     const cellHeight = boxHeight / 3;
 
     let cellsHTML = `
-    <div id="CELL_${SudokuMap.boxIndices[box][0]}"></div>
-    <div id="CELL_${SudokuMap.boxIndices[box][1]}"></div>
-    <div id="CELL_${SudokuMap.boxIndices[box][2]}"></div>
-    <div id="CELL_${SudokuMap.boxIndices[box][3]}"></div>
-    <div id="CELL_${SudokuMap.boxIndices[box][4]}"></div>
-    <div id="CELL_${SudokuMap.boxIndices[box][5]}"></div>
-    <div id="CELL_${SudokuMap.boxIndices[box][6]}"></div>
-    <div id="CELL_${SudokuMap.boxIndices[box][7]}"></div>
-    <div id="CELL_${SudokuMap.boxIndices[box][8]}"></div>`
+    <div id="CELL_${SudokuMap.boxIndices[box][0]}" class="cellDefault"></div>
+    <div id="CELL_${SudokuMap.boxIndices[box][1]}" class="cellDefault"></div>
+    <div id="CELL_${SudokuMap.boxIndices[box][2]}" class="cellDefault"></div>
+    <div id="CELL_${SudokuMap.boxIndices[box][3]}" class="cellDefault"></div>
+    <div id="CELL_${SudokuMap.boxIndices[box][4]}" class="cellDefault"></div>
+    <div id="CELL_${SudokuMap.boxIndices[box][5]}" class="cellDefault"></div>
+    <div id="CELL_${SudokuMap.boxIndices[box][6]}" class="cellDefault"></div>
+    <div id="CELL_${SudokuMap.boxIndices[box][7]}" class="cellDefault"></div>
+    <div id="CELL_${SudokuMap.boxIndices[box][8]}" class="cellDefault"></div>`
 
     return cellsHTML;
 }
 
 function fillCellCandidatesType1(cell, candidates) {
     
-    const candidateHeight = cellsArray[cell].getBoundingClientRect().height / 3-1;
-
     let candidateHTML = `
-<table style="width:100%;" class="box_table">
-<tr style="line-height: ${candidateHeight}px; height: ${candidateHeight}px;">
-<td class="cell_candidate">${candidates[0]}</td>
-<td class="cell_candidate">${candidates[1]}</td>
-<td class="cell_candidate">${candidates[2]}</td>
-</tr>
-<tr style="line-height: ${candidateHeight}px; height: ${candidateHeight}px;">
-<td class="cell_candidate">${candidates[3]}</td>
-<td class="cell_candidate">${candidates[4]}</td>
-<td class="cell_candidate">${candidates[5]}</td>
-</tr>
-<tr style="line-height: ${candidateHeight}px; height: ${candidateHeight}px;">
-<td class="cell_candidate">${candidates[6]}</td>
-<td class="cell_candidate">${candidates[7]}</td>
-<td class="cell_candidate">${candidates[8]}</td>
-</tr>
-</table>`
+    <div class="candidateType1">${candidates[0]}</div>
+    <div class="candidateType1">${candidates[1]}</div>
+    <div class="candidateType1">${candidates[2]}</div>
+    <div class="candidateType1">${candidates[3]}</div>
+    <div class="candidateType1">${candidates[4]}</div>
+    <div class="candidateType1">${candidates[5]}</div>
+    <div class="candidateType1">${candidates[6]}</div>
+    <div class="candidateType1">${candidates[7]}</div>
+    <div class="candidateType1">${candidates[8]}</div>`
 
+    cellsArray[cell].className = 'cellCandidateType1';
     cellsArray[cell].innerHTML = candidateHTML;
-
 }
 
 function fillCellCandidatesType2(cell, candidates)
@@ -58,59 +50,109 @@ function fillCellCandidatesType2(cell, candidates)
     cellsArray[cell].innerHTML=candidates
 }
 
+let mouseDown = false;
+let lastSelectedCell = undefined;
+// evt.button -> button associated with event: 0->Left; 1->Center; 2->Right
+// evt.buttons -> bitmask of the currently pressed buttons: bit[0]->Left; bit[1]->Right; bit[2]->Center
 function addMouseEventListener(elementId, event)
 {
-        const element=document.getElementById(elementId)
-        element.addEventListener(event, evt => {
+    const element=document.getElementById(elementId)
+    element.addEventListener(event, evt => {
         evt.preventDefault();
         evt.stopPropagation();
-        // console.log("Element " + elementId + " mousemouve " + evt.clientX);
-        // console.log("squareTable  mousemove clientX: " + evt.clientX + ", clientY: " + evt.clientY);
-        // console.log(evt.type);
-        // const onCell = getCellId_v2(evt.clientX,evt.clientY);
-        const onCell = getCellId(evt.clientX,evt.clientY);
-        if (onCell !== undefined)
+
+        // Ignore mouseup or mousedown events for buttons other than left (button 0)
+        if ((event === 'mousedown' || event === 'mouseup') && (evt.button !== 0))
+            return;
+
+        // Get the cell under the mouse
+        const onCell = getCellId(evt.clientX, evt.clientY);
+
+        if (onCell === undefined)
+            return
+
+        if (event === 'mousedown')
         {
-            if (evt.buttons === 1)
-                cellsArray[onCell].style.backgroundColor = "blue"
-            if (evt.buttons === 2)
-                cellsArray[onCell].style.backgroundColor = ""
+            selectionFirstCell(onCell);
+            lastSelectedCell = onCell;
         }
-        console.log("Mouse on cell " + onCell);
+        if (event === 'mouseup')
+        {
+            lastSelectedCell = undefined;
+        }
+        if (event === 'mousemove' && onCell !== lastSelectedCell && lastSelectedCell !== undefined)
+        {
+            selectionNextCell(onCell);
+            lastSelectedCell = onCell;
+        }
+
+        // console.log("Mouse on cell " + onCell);
+        // console.log("Element " + elementId + " received event " + event + " button " + evt.button + " buttons " + evt.buttons);
         t=document.getElementById("logParagraph");
         // t.textContent="squareTable  mousemove" + " clientX: " + evt.clientX + ", clientY: " + evt.clientY
         t.textContent="Element " + elementId + " received event " + event + " clientX: " + evt.clientX + ", clientY: " + evt.clientY + ", button: " + evt.button + ", buttons: " + evt.buttons
-        // t.textContent="Mouse on cell " + onCell
     });
 }
 
 function addTouchEventListener(elementId, event)
 {
-        const element=document.getElementById(elementId)
-        element.addEventListener(event, evt => {
+    const element=document.getElementById(elementId)
+    element.addEventListener(event, evt => {
         evt.preventDefault();
         evt.stopPropagation();
-        // console.log("Element " + elementId + " mousemouve " + evt.clientX);
-        // console.log("squareTable  mousemove clientX: " + evt.clientX + ", clientY: " + evt.clientY);
-        // console.log(evt.type);
 
+        // Ignore mouseup or mousedown events for buttons other than left (button 0)
+        // if ((event === 'mousedown' || event === 'mouseup') && (evt.button !== 0))
+        //     return;
+
+        if (event === 'touchend')
+        {
+            lastSelectedCell = undefined;
+            return;
+        }
+
+        // Get the cell under the mouse
         const x = evt.touches[0].clientX;
         const y = evt.touches[0].clientY;
+        const onCell = getCellId(x, y);
 
-        const onCell = getCellId_v2(x, y);
+        if (onCell === undefined)
+            return
 
-        if (onCell !== undefined)
+        if (event === 'touchstart')
         {
-            cellsArray[onCell].style.backgroundColor = "blue"
-            // if (evt.buttons === 1)
-            // if (evt.buttons === 2)
-            //     cellsArray[onCell].style.backgroundColor = ""
+            selectionFirstCell(onCell);
+            lastSelectedCell = onCell;
         }
-        console.log("Mouse on cell " + onCell);
+        if (event === 'touchmove' && onCell !== lastSelectedCell && lastSelectedCell !== undefined)
+        {
+            selectionNextCell(onCell);
+            lastSelectedCell = onCell;
+        }
+
         t=document.getElementById("logParagraph");
         // t.textContent="squareTable  mousemove" + " clientX: " + evt.clientX + ", clientY: " + evt.clientY
         t.textContent="Element " + elementId + " received event " + event + " clientX: " + x + ", clientY: " + y
         // t.textContent="Mouse on cell " + onCell
+    });
+}
+
+function addButtonListener(elementId, buttonNumber)
+{
+    const element=document.getElementById(elementId)
+    element.addEventListener('click', evt => {
+        evt.preventDefault();
+        evt.stopPropagation();
+
+        for (cell of selectedCellsSet) {
+            cellSetValue(cell, buttonNumber)
+        }
+        // if (lastSelectedCell)
+        //     cellSetValue(lastSelectedCell, buttonNumber)
+
+        console.log("Button " + buttonNumber + " Clicked");
+        t=document.getElementById("logParagraph");
+        t.textContent="Button " + buttonNumber + " Clicked"
     });
 }
 
@@ -160,28 +202,9 @@ function cellSetValue(cell, value)
 {
     cellsArray[cell].style.fontSize = "";
     cellsArray[cell].innerHTML = value;
+    cellsArray[cell].className = 'cellDefault';
 }
 
-function getTextWidth(textToMeasure, fontSize) {
-
-    text = document.createElement("span");
-    document.body.appendChild(text);
-
-    text.style.fontFamily = "verdana";
-    text.style.fontSize = fontSize;
-    text.style.height = 'auto';
-    text.style.width = 'auto';
-    text.style.position = 'absolute';
-    // text.style.whiteSpace = 'no-wrap';
-    text.innerHTML = textToMeasure;
-
-    width = Math.ceil(text.clientWidth);
-    formattedWidth = width + "px";
-
-    document.body.removeChild(text);
-
-    return formattedWidth
-}
 
 
 
@@ -208,11 +231,22 @@ for (let i=0; i<81; i++)
     cellsArray.push(document.getElementById(`CELL_${i}`));
 }
 
+// Number Buttons Array
+// const numBtnArray = [];
+for (let i=1; i<=9; i++)
+{
+    // numBtnArray.push(document.getElementById(`BTN_${i}`));
+    addButtonListener(`BTN_${i}`, i)
+}
+
 
 addMouseEventListener("sudokuBoard", "mousemove")
 addMouseEventListener("sudokuBoard", "mousedown")
+addMouseEventListener("sudokuBoard", "mouseup")
+addMouseEventListener("body", "mouseup")
 addTouchEventListener("sudokuBoard",  "touchmove")
 addTouchEventListener("sudokuBoard",  "touchstart")
+addTouchEventListener("sudokuBoard",  "touchend")
 
 
 cellSetBackgroundColor(0, "red")
@@ -226,5 +260,33 @@ for (let i=0; i<81; i++)
     cellValue = cellValue === "0" ? "" : cellValue;
     cellSetValue(i, cellValue);
 }
-// fillCellCandidatesType1(72, ['','','','','','',7,8,9])
+fillCellCandidatesType1(72, ['','',3,'',5,'',7,8,9])
 fillCellCandidatesType2(73, "123")
+fillCellCandidatesType2(74, "12345")
+
+
+
+// board selection APIs:
+// Selection Start -> cell Id : call when the first/only cell is selected
+// Cell Selected -> cell Id :  call when more cells are selected after first cell
+
+let selectedCellsSet = new Set();
+let selectionFirstCell = cellId =>
+{
+    selectedCellsSet.clear();
+    selectedCellsSet.add(cellId);
+    console.log("selectionFirstCell " + cellId);
+
+    for (cell of cellsArray)
+        cell.style.backgroundColor = ""
+
+    cellsArray[cellId].style.backgroundColor = "lavender"
+};
+
+// function selectionNextCell(cellId)
+let selectionNextCell = cellId =>
+{
+    selectedCellsSet.add(cellId);
+    console.log("selectionNextCell " + cellId);
+    cellsArray[cellId].style.backgroundColor = "lavender"
+}
